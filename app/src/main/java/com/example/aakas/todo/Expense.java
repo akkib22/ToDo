4688b4ac98a505609620fd725e4c2536e8b3786f
aakash bhati <aakashbhati.com@gmail.com>
2018-07-18 10:11:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aakas/todo/Expense.java
@@ -0,0 +1,28 @@
+package com.example.aakas.todo;
+
+public class Expense {
+
+    private String name;
+    private int amount;
+
+    public Expense(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
